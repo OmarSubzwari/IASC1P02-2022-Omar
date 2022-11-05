6add29f6caf7dcd552ace63c7db574835fa76453
OmarSubzwari <81269168+OmarSubzwari@users.noreply.github.com>
2022-11-05 03:10:49 -0400

    working on c2 lab8

--- Labs/Lab8/scriptstwo.js
+++ Labs/Lab8/scriptstwo.js
@@ -5,13 +5,13 @@ document.getElementById("red").style.color="red";
 
 //A function to change colours of elements when called
 function mix(){
-	
+
 	var first = "#00BB00";
-	var second = "BB9320";
+	var second = "#BB9320";
 	var third = "#AB00FF";
-	
+
 	document.getElementById("blue").style.color=first;
-	document.getElementBy"green").style.colour=second;
+	document.getElementById("green").style.colour=second;
 	document.getElementById("red").style.color=third;
-	
+
 }
