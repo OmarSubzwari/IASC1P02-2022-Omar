51ec419073c4bdcc5c6bcfee31ffda586df98d87
OmarSubzwari <81269168+OmarSubzwari@users.noreply.github.com>
2022-11-05 02:28:58 -0400

    started to work on C2 for lab8

---
+++ Labs/Lab8/scriptstwo.js
@@ -0,0 +1,17 @@
+//Sets the initial colours of the three elements on page load
+document.getElementById("blue").style.color="blue";
+document.getElementById("green").style.color="green";
+document.getElementById("red").style.color="red";
+
+//A function to change colours of elements when called
+function mix(){
+	
+	var first = "#00BB00";
+	var second = "BB9320";
+	var third = "#AB00FF";
+	
+	document.getElementById("blue").style.color=first;
+	document.getElementBy"green").style.colour=second;
+	document.getElementById("red").style.color=third;
+	
+}
