d13a725e12450ad23827aca78cae44fd6d4d9b7b
OmarSubzwari <81269168+OmarSubzwari@users.noreply.github.com>
2022-11-05 22:03:26 -0400

    started to work on c3 lab 8

---
+++ Labs/Lab8/scriptsthree.js
@@ -0,0 +1,8 @@
+//This will output when the page loads
+document.write("Output Zero Here");
+
+//This will output when a button is pressed
+function therest(){
+	document.ElementById("output3").innerHTML="Output Three Here";
+}
+
--- Labs/Lab8/scriptstwo.js
+++ Labs/Lab8/scriptstwo.js
@@ -2,15 +2,13 @@
 document.getElementById("blue").style.color="blue";
 document.getElementById("green").style.color="green";
 document.getElementById("red").style.color="red";
-
 //A function to change colours of elements when called
 function mix(){
-
 	var first = "#00BB00";
 	var second = "#BB9320";
 	var third = "#AB00FF";
 
-	document.getElementById("blue").style.color=first;
+	document.getElementById("blue").style.color= first;
 	document.getElementById("green").style.colour=second;
 	document.getElementById("red").style.color=third;
 
