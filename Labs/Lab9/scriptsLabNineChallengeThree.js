c58262adaa9a06394a0536e62d1d77f1b8e6e8cf
OmarSubzwari <81269168+OmarSubzwari@users.noreply.github.com>
2022-11-12 22:35:03 -0500

    continued to work on lab 9

---
+++ Labs/Lab9/scriptsLabNineChallengeThree.js
@@ -0,0 +1,9 @@
+function getCalculation(){
+  num1 = document.getElementById("basicTextArea").value;
+  num2 = document.getElementById("basicTextAreaTwo").value;
+  document.getElementById("label").innerHTML = String(num1) + String(num2);
+}
+
+
+var num1 = document.getElementById("basicTextArea").value;
+var num2 = document.getElementById("basicTextAreaTwo").value;
