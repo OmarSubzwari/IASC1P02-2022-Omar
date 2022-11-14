9db754360c2607c6d0055cbfaedbc07657eb344b
OmarSubzwari <81269168+OmarSubzwari@users.noreply.github.com>
2022-11-13 23:54:01 -0500

    completed lab 9

--- Labs/Lab9/scriptsLabNineChallengeTwo.js
+++ Labs/Lab9/scriptsLabNineChallengeTwo.js
@@ -6,3 +6,16 @@ document.getElementById("theYear").innerHTML = "Your birth year is " + (2022 - m
 
 var myPrompt = prompt("What is your age?");
 console.log(myPrompt);
+
+//citation for null that is used in javascript code.
+//https://stackoverflow.com/questions/71075523/asking-the-user-to-re-enter-year-of-birth-until-birth-year-current-year // null idea was used from user "Kato" on the website stack overflow/
+
+
+//var start = new Date();
+//var todayMonth = start.getMonth() + 1 //fix for zero-based month index
+//var todayDate = start.getDate();
+//var todayDay = start.getDay();
+//var todayHour = start.getHours();
+//var todayYear = start.getYear();
+//var todayFullYear = start.getFullYear();
+//var todaySeconds = start.getSeconds();
