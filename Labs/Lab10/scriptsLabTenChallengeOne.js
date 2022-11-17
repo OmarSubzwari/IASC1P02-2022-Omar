fbd9448bd5a189fc7cd5f81536add2595d1e592b
OmarSubzwari <81269168+OmarSubzwari@users.noreply.github.com>
2022-11-17 00:11:18 -0500

    continuning to work on lab 10

--- Labs/Lab10/scriptsLabTenChallengeOne.js
+++ Labs/Lab10/scriptsLabTenChallengeOne.js
@@ -1,7 +1,41 @@
-function getCalculation(){
+//addition function
+function getAddition(){
   num1 = document.getElementById("basicTextArea").value;
   num2 = document.getElementById("basicTextAreaTwo").value;
   document.getElementById("label").innerHTML = Number(num1) + Number(num2);
+}
+
+
+var num1 = document.getElementById("basicTextArea").value;
+var num2 = document.getElementById("basicTextAreaTwo").value;
+
+//subtraction function
+function getSubtraction(){
+  num1 = document.getElementById("basicTextArea").value;
+  num2 = document.getElementById("basicTextAreaTwo").value;
+  document.getElementById("label").innerHTML = Number(num1) - Number(num2);
+}
+
+
+var num1 = document.getElementById("basicTextArea").value;
+var num2 = document.getElementById("basicTextAreaTwo").value;
+
+//division function
+function getDivision(){
+  num1 = document.getElementById("basicTextArea").value;
+  num2 = document.getElementById("basicTextAreaTwo").value;
+  document.getElementById("label").innerHTML = Number(num1) / Number(num2);
+}
+
+
+var num1 = document.getElementById("basicTextArea").value;
+var num2 = document.getElementById("basicTextAreaTwo").value;
+
+//multiplication function
+function getMultiplication(){
+  num1 = document.getElementById("basicTextArea").value;
+  num2 = document.getElementById("basicTextAreaTwo").value;
+  document.getElementById("label").innerHTML = Number(num1) * Number(num2);
 }
 
 
