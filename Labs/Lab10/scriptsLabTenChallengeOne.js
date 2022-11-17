4c5b1f073f03d1925d991b3cba7baf92073270eb
OmarSubzwari <81269168+OmarSubzwari@users.noreply.github.com>
2022-11-16 23:23:18 -0500

    Started to work on lab ten

---
+++ Labs/Lab10/scriptsLabTenChallengeOne.js
@@ -0,0 +1,9 @@
+function getCalculation(){
+  num1 = document.getElementById("basicTextArea").value;
+  num2 = document.getElementById("basicTextAreaTwo").value;
+  document.getElementById("label").innerHTML = Number(num1) + Number(num2);
+}
+
+
+var num1 = document.getElementById("basicTextArea").value;
+var num2 = document.getElementById("basicTextAreaTwo").value;
