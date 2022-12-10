23a7fdaa94d580bee64aafadfd8e1e3876b2d52a
OmarSubzwari <81269168+OmarSubzwari@users.noreply.github.com>
2022-12-10 04:17:10 -0500

    added slideshow into accessories page showing more images

--- Labs/FinalWebsite/scriptsfinal.js
+++ Labs/FinalWebsite/scriptsfinal.js
@@ -19,3 +19,31 @@ function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
+
+//this is the JS for accessories.html page
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
